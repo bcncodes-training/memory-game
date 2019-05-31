93845405ed013848d8352610165bce6a26006f64
Dani <neollob@yahoo.es>
2019-05-31 22:00:53 +0200

    6th commit - Shuffle refactored

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -3,17 +3,17 @@ class MemoryGame {
     this.cards = cards;
   };
 
-  shuffleCard(cardsArr) {// recibe un mazo de cartas y devuelve otro con las cartas barajadas
-      let shuffled=[];
-      /// 'forEach' mode
-      cardsArr.forEach(e => {// Cada carta de 'cardsArr' la añadimos al nuevo array de cartas 'shuffled' en una posicion aleatoria
-        shuffled.splice(Math.floor(Math.random()*shuffled.length),0,e);
-      });
-      /// 'for' mode
-      /*for (let i = 0; i < cardsArr.length; i++) {// Cada carta de 'cardsArr' la añadimos al nuevo array de cartas 'shuffled' en una posicion aleatoria
-        shuffled.splice(Math.floor(Math.random()*shuffled.length),0,cardsArr[i]);
-      }*/
-      return shuffled;
+  shuffleCard(cardsArr) {// recibe un array de cartas y devuelve otro con las cartas barajadas
+    let shuffled = [];
+    /// 'forEach' mode
+    cardsArr.forEach(e => {// Cada carta de 'cardsArr' la añadimos al nuevo array de cartas 'shuffled' en una posicion aleatoria
+      shuffled.splice(Math.floor(Math.random() * (shuffled.length + 1)), 0, e);
+    });
+    /// 'for' mode
+    /*for (let i = 0; i < cardsArr.length; i++) {// Cada carta de 'cardsArr' la añadimos al nuevo array de cartas 'shuffled' en una posicion aleatoria
+      shuffled.splice(Math.floor(Math.random()*(shuffled.length+1)),0,cardsArr[i]);
+    }*/
+    return shuffled;
   };
 
   checkIfPair(firstCard, secondCard) { // comprueba si las dos cartas son iguales y devuelve true o false
